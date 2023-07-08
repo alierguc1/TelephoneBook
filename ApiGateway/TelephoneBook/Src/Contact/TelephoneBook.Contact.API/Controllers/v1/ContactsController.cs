cd40baf2d9a942e09e1af85134946b8b52e5d505
Ali Ergüç <ergucali1@gmail.com>
2023-07-08 15:04:46 +0300

    Create Contacts Controller and Api Versioning
    Create Contacts Controller and add ApiVersioning package.

---
+++ ApiGateway/TelephoneBook/Src/Contact/TelephoneBook.Contact.API/Controllers/v1/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace TelephoneBook.Contact.API.Controllers.v1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class ContactsController : ControllerBase
+    {
+       
+        [HttpGet]
+        public string Get() => ".Net Core Web API Version 2";
+    }
+}
